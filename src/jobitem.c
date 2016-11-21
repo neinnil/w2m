d3037298dfc7c1ad8a2e9486bee18ba0ee94e368
neinnil <neinnil@gmail.com>
2016-11-21 16:39:15 +0900

    remove jobitem struct from jobitem.c

--- src/jobitem.c
+++ src/jobitem.c
@@ -21,19 +21,6 @@
 #include <errno.h>
 #include "jobitem.h"
 
-struct _jobitem {
-	int				state;	/* none (not started), doing, done */	
-#define WORK_NOT_STARTED	0
-#define WORK_DOING			1
-#define WORK_DONE			2
-#define WORK_ABORTED		4
-	char			*src;
-	char			*dst;
-	int				isSupported; /* 0: not supported. 1: supported. */
-	void			*pcmInfo;
-	pthread_mutex_t	*lock;
-}
-
 jobitem_t*	alloc_jobitem(char *src)
 {
 	int			rc = 0;
