ee638b5e37104a1e81d76774eb73effe13b604d2
neinnil <neinnil@gmail.com>
2016-12-12 22:04:03 +0900

    get the length of file.

--- pretest/winfo.c
+++ pretest/winfo.c
@@ -7,6 +7,13 @@ int main (int ac, char** av)
 	WAVE_FILE_INFO_T * info = NULL;
 
 	if (NULL!=(info = getWaveInfoFromFile(av[1]))) {
+		long length = 0;
+		FILE *fp = fopen (av[1], "r");
+		fseek(fp, 0L, SEEK_END);
+		length = ftell(fp);
+		fclose (fp);
+		printf("\nFile: %s\n", av[1]);
+		printf("length: %ld\n", length);
 		printWaveInfo(info);
 		FREEWAVEFILEINFO(info);
 	}
