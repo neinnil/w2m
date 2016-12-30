ff866ec166a5dd74cdce57860014f69d89bbbe37
neinnil <neinnil@gmail.com>
2016-12-31 01:25:48 +0900

    for linux.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #/bin/bash
 TOP_DIR=$PWD
 
-
-function install_lame {
+install_lame() {
 echo "install libmp3lame."
 cd ext_src
 tar xf lame-3.99.5.tar.bz2
@@ -11,13 +10,13 @@ cd lame-3.99.5
 [[ $? -eq 0 ]] && echo "configured: "$? 
 make -j3 install
 echo "install: $?"
-if [[ $? -eq 0 ]] ; then
+if [ $? -eq 0 ] ; then
 touch $TOP_DIR/ext_src/.installed
 fi
 }
 
 
-if [[ ! -d $TOP_DIR/ext_lib || ! -e $TOP_DIR/ext_src/.installed ]] ; then
+if [ ! -d $TOP_DIR/ext_lib || ! -e $TOP_DIR/ext_src/.installed ] ; then
 install_lame
 fi
 
