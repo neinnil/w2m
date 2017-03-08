06cdd7d6a7d1601a1ebaa2106858be2c6323ae85
neinnil <neinnil@gmail.com>
2017-03-08 09:39:34 +0900

    apply two arguments, clean and w2m.

--- build.sh
+++ build.sh
@@ -1,25 +1,42 @@
 #/bin/bash
 TOP_DIR=$PWD
+export TOP_DIR
 
 install_lame() {
-echo "install libmp3lame."
-cd ext_src
-tar xf lame-3.99.5.tar.bz2
-cd lame-3.99.5
-./configure --prefix $TOP_DIR/ext_lib
-[[ $? -eq 0 ]] && echo "configured: "$? 
-make -j3 install
-echo "install: $?"
-if [ $? -eq 0 ] ; then
-touch $TOP_DIR/ext_src/.installed
-fi
+	echo "install libmp3lame."
+	cd ext_src
+	tar xf lame-3.99.5.tar.bz2
+	cd lame-3.99.5
+	./configure --prefix $TOP_DIR/ext_lib
+	[[ $? -eq 0 ]] && echo "configured: "$? 
+	make -j3 install
+	echo "install: $?"
+	if [ $? -eq 0 ] ; then
+	touch $TOP_DIR/ext_src/.installed
+	fi
 }
 
 if [[ ! -d $TOP_DIR/ext_lib || ! -e $TOP_DIR/ext_src/.installed ]] ; then
-install_lame
+	install_lame
+	cd $TOP_DIR
 fi
 
-cd $TOP_DIR
-cd ./build
-make w2m
+if [ $# -gt 0 ] ; then
+	case $1 in 
+		clean)
+			echo "Clean objects"
+			make -C build clean
+			;;
+		w2m)
+			echo "build w2m"
+			make -C build w2m
+			;;
+		*)
+			echo "./build.sh [clean|w2m]"
+			;;
+	esac
+else
+	make -C build w2m
+fi
+
 
